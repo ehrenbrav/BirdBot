870901f24b5f3d96b71c3f979ad131dd8c109527
Ehren J. Brav <ehrenbrav@yahoo.com>
2014-06-16 19:24:23 -0700

    Started to add ARFF file encoder.

--- src/BirdBotYaafe.py
+++ src/BirdBotYaafe.py
@@ -2,11 +2,14 @@
 
 import wav_file_importer 
 import yaafe_feature_extractor
+import arff
 import cPickle
 
 sample_frequency, data = wav_file_importer.validate_and_read_file()
 
 features = yaafe_feature_extractor.extract(sample_frequency, data)
+
+arff_file = arff.dumps(features)
 
 print features
 #cPickle.dump(features, open("features.obj", "wb"))
