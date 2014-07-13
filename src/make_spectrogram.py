1c738d3ab49c43a5be61e4acab819d297c4963cf
Ehren J. Brav <ehrenbrav@yahoo.com>
2014-07-13 10:46:11 -0700

    Spectrogram is now generating images of 3 second durations of an audio file.

--- src/make_spectrogram.py
+++ src/make_spectrogram.py
@@ -8,42 +8,45 @@ spectrogram frame size, then starts with an offset equal to the
 remainder, to ensure that the entire wav file is covered.
 """
 
-import os
-import wave
+#pylint: disable=C0103
+
+import wav_file_importer as wfi
 import argparse
 import matplotlib as mpl
 # Force matplotlib to not use any Xwindows backend.
 mpl.use('Agg')
 import pylab
-import numpy as np
 
 GRAPH_WIDTH = 1024
 GRAPH_HEIGHT = 128
+SAMPLE_DURATION = 3
 
-def graph_spectrogram(wav_file):
-    data, frame_rate = get_wav_info(wav_file)
-    Pxx, freqs, time, im = pylab.specgram(data, Fs=frame_rate)
-    remainder = Pxx.shape[1] % GRAPH_WIDTH
-    first_section = Pxx[:, 0:Pxx.shape[1] - remainder]
-    pieces = np.split(first_section, first_section.shape[1]/GRAPH_WIDTH, axis=1)
+def graph_spectrogram(path):
+    """
+    Create the spectrograms and save as png files.
+    """
+
+    # Load the wav file.
+    sample_rate, full_audio_data = wfi.validate_and_read_file(path)
+    frame_size = sample_rate * SAMPLE_DURATION
+    remainder = len(full_audio_data) % frame_size
+    print remainder
+
+    # Chop the wav file into equally sized pieces, starting from
+    # the front, then do the same starting from the back.
+    front_samples = [full_audio_data[i:i+frame_size] \
+            for i in range(0, len(full_audio_data) - remainder, frame_size)]
+
+    end_samples = [full_audio_data[i:i+frame_size] \
+            for i in range(remainder, len(full_audio_data), frame_size)]
+
+    # Make the spectrograms.
     counter = 0
-    for piece in pieces:
-        pylab.plot(piece[0], piece[1])
+    for sample in front_samples + end_samples:
+
+        pylab.specgram(sample, Fs=sample_rate)
         pylab.savefig('spectrogram' + str(counter) + '.png')
-        counter += 1
-
-def get_wav_info(wav_file):
-    wav = wave.open(wav_file, 'r')
-    frames = wav.readframes(-1)
-    sound_info = pylab.fromstring(frames, 'Int16')
-    frame_rate = wav.getframerate()
-    wav.close()
-
-    if frame_rate != 44100:
-        print "Error: frame rate is not 44100."
-        exit(1)
-    return sound_info, frame_rate
-
+        counter = counter + 1
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
